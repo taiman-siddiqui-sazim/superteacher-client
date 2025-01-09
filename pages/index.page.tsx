ebcd7387670fdb0b263fb83af7dba888aea16ad8
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2025-01-09 17:15:44 +0600

    feat(st-19): match sazim ui and make landing page responsive

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -19,14 +19,14 @@ export default function Home() {
       <NextHead />
 
       <div className="flex justify-center items-center h-screen">
-        <Card className="p-4 flex flex-col items-center">
+        <Card className="w-3/5 p-4 flex flex-col items-center border-transparent">
           <CardHeader>
-            <CardTitle className="text-3xl font-bold mb-4 text-white">
+            <CardTitle className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 text-white">
               WELCOME TO SUPERTEACHER
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-lg mb-4 text-white">Where learning and teaching come together!</p>
+            <p className="text-base sm:text-lg md:text-xl mb-4 text-white">Where learning and teaching come together!</p>
           </CardContent>
           <div className="flex space-x-4">
             <Button
